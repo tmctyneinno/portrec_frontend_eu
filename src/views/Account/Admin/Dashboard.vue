9c1894089f99cf8cf1c548f308621a2799aaec52
emekaJoseph <josephiwuji@gmail.com>
2024-05-08 17:59:24 +0100

    dashboard in progress

--- src/views/Account/Admin/Dashboard.vue
+++ src/views/Account/Admin/Dashboard.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="row g-3 p-lg-3 bg-light m-0 pb-5">
         <div class="col-12">
-            <div class="row">
+            <div class="row g-3">
                 <div class="col-12">
-                    <div class="fw-bold text-capitalize">{{ useFxn.greet() }}, {{ profileStore.data?.name ?? '' }}</div>
-                    <span class="text-muted">Here is what’s happening with your job search applications from
+                    <div class="fw-bold text-capitalize">{{ useFxn.greet() }}, Admin</div>
+                    <span class="text-muted">Here is your job listings statistic report from
                         <span class="fw-bold">{{ dateRange ? date_display(dateRange) : '' }}</span>.
                     </span>
                     <span class="float-start float-lg-end" style="width: 180px;">
@@ -17,75 +17,176 @@
             </div>
         </div>
 
-        <div class="col-md-3">
+
+        <div class="col-lg-4">
+            <div class="card colored-card card-color1 shadow-sm">
+                <div class="card-body">
+                    <div class="big-number">76</div>
+                    <div class=" hover-tiltY cursor-pointer">New candidates to review
+                        <span class="float-end ">
+                            <i class="bi bi-chevron-right  "></i>
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card colored-card card-color2 shadow-sm">
+                <div class="card-body">
+                    <div class="big-number">3</div>
+                    <div class=" hover-tiltY cursor-pointer">Schedule for today
+                        <span class="float-end">
+                            <i class="bi bi-chevron-right"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card colored-card card-color3 shadow-sm">
+                <div class="card-body">
+                    <div class="big-number">24</div>
+                    <div class=" hover-tiltY cursor-pointer">Messages received
+                        <span class="float-end">
+                            <i class="bi bi-chevron-right"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-8">
+            <div class="card shadow-sm border-0">
+                <div class=" card-header  bg-transparent fw-bold border-0">
+                    Job statistics <br>
+                    <small class="text-muted">Showing Job statistic from
+                        {{ dateRange ? date_display(dateRange) : '' }}</small>
+                </div>
+                <div class="card-body">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="tab1-tab" data-bs-toggle="tab" data-bs-target="#tab1"
+                                type="button" role="tab" aria-controls="tab1" aria-selected="true">
+                                Overview
+                            </button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="tab2-tab" data-bs-toggle="tab" data-bs-target="#tab2"
+                                type="button" role="tab" aria-controls="tab2" aria-selected="false">
+                                Jobs View
+                            </button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="tab3-tab" data-bs-toggle="tab" data-bs-target="#tab3"
+                                type="button" role="tab" aria-controls="tab3" aria-selected="false">
+                                Jobs Applied
+                            </button>
+                        </li>
+                    </ul>
+
+                    <div class="tab-content" style="min-height: 300px;">
+                        <!-- TAB1 -->
+                        <div class="tab-pane active p-3 py-4" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
+                            <div class="row g-2">
+                                <div class="col-lg-8">
+                                    <div class="card h-100">
+                                        <div id="chart">
+                                            <apexchart type="bar" :options="chartOptions_1"
+                                                :series="chartOptions_1.series">
+                                            </apexchart>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="row g-3">
+                                        <div class="col-12">
+                                            <div class="card info-card-tab1 ">
+                                                <div class="card-header bg-transparent fw-bold border-0">
+                                                    Job Views
+                                                    <span class="float-end float-icon-card color1">
+                                                        <i class="bi bi-eye"></i>
+                                                    </span>
+                                                </div>
+                                                <div class="card-body pt-0">
+                                                    <div class="big-number">2,345</div>
+                                                    <div class="text-muted small">This week
+
+                                                        <span class="text-success fw-bold">
+                                                            65.4% <i class="bi bi-arrow-up"></i>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="card info-card-tab1 ">
+                                                <div class="card-header bg-transparent fw-bold border-0">
+                                                    Job Applied
+                                                    <span class="float-end float-icon-card color2">
+                                                        <i class="bi bi-journal-text"></i>
+                                                    </span>
+                                                </div>
+                                                <div class="card-body pt-0">
+                                                    <div class="big-number">654</div>
+                                                    <div class="text-muted small">This week
+
+                                                        <span class="text-danger fw-bold">
+                                                            0.5% <i class="bi bi-arrow-down"></i>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <!-- TAB2 -->
+                        <div class="tab-pane p-3 py-4" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
+                            Jobs View
+                        </div>
+
+                        <!-- TAB3 -->
+                        <div class="tab-pane p-3 py-4" id="tab3" role="tabpanel" aria-labelledby="tab3-tab">
+                            Jobs Applied
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
             <div class="row g-3">
                 <div class="col-12">
-                    <div class="card colored-card total-jobs-card shadow-sm">
-                        <div class="card-body">
-                            <div>Total Jobs Applied</div>
-                            <div class="big-number">45</div>
+                    <div class="card">
+                        <div class="card-header border-0 bg-transparent fw-bold">
+                            Jobs Open
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="fw-bold" style="font-size: 2.6rem;">12
+                                <span class=" fs-6 text-muted fw-lighter">Jobs Opened</span>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-12">
-                    <div class="card colored-card interviewed-card shadow-sm">
-                        <div class="card-body">
-                            <div>Interviewed</div>
-                            <div class="big-number">18</div>
+                    <div class="card">
+                        <div class="card-header border-0 bg-transparent fw-bold">
+                            Applicants Summary
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="fw-bold" style="font-size: 2.6rem;">67
+                                <span class=" fs-6 text-muted fw-lighter">Applicants</span>
+                            </div>
+
+                            <apexchart type="bar" :options="chartOptions_2" :series="chartOptions_2.series">
+                            </apexchart>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card shadow-sm">
-                <div class="color-green card-header py-2 pt-3 bg-transparent fw-bold border-0">
-                    Jobs Applied
-                    Status</div>
-                <div class="card-body">
-                    <div class="row g-3">
-                        <div class="col-lg-5 d-flex justify-content-lg-center align-items-center">
-                            <div id="chart ">
-                                <apexchart type="donut" :options="chartOptions" :series="chartOptions.series">
-                                </apexchart>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-7 py-lg-5 xsmall row justify-content-center align-items-center">
-                            <div class="col-6 col-lg-12 bg-succes">
-                                <i class="color-green bi bi-square-fill"></i>
-                                <span class="fw-bold color-green mx-1">60%</span>
-                                <span class="text-muted">Unsuitable</span>
-                            </div>
-                            <div class="col-6 col-lg-12">
-                                <i class="color-faint bi bi-square-fill"></i>
-                                <span class="fw-bold color-green mx-1">40%</span>
-                                <span class="text-muted">Interviewed</span>
-                            </div>
-                        </div>
-
-
-                    </div>
-                    <div class="col-12 mt-3">
-                        <span class="fw-bold theme-color cursor-pointer float-lg-end">View All Applications <i
-                                class="bi bi-arrow-right"></i></span>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <div class="col-md-5">
-            <div class="card shadow-sm">
-                <div class="card-header py-2 fw-bold bg-transparent">Upcomming Interviews</div>
-                <div class="card-header py-2  bg-transparent shadow-sm">
-                    Today, 26 November
-                    <span class="float-end">
-                        <i class="bi bi-chevron-left"></i>
-                        <i class="bi bi-chevron-right"></i>
-                    </span>
-                </div>
-                <div class="card-body row gy-3">
-
                 </div>
             </div>
         </div>
@@ -129,14 +230,11 @@ function setDateRange() {
 }
 
 
-
-
-
-
 // chart
-const chartOptions = {
+const chartOptions_1 = {
     chart: {
-        type: 'donut',
+        type: 'bar',
+        stacked: true,
     },
     dataLabels: {
         enabled: false
@@ -146,46 +244,90 @@ const chartOptions = {
             enabled: false,
         }
     },
-    colors: ['#1EB0B4', '#E9EBFD'],
-    series: [40, 60],
-    // labels: ['Apples', 'Oranges', 'Bananas', 'Grapes', 'Mangoes'],
+    zoom: {
+        enabled: false
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            legend: {
+                position: 'bottom',
+                offsetX: -10,
+                offsetY: 0
+            }
+        }
+    }],
+    xaxis: {
+        categories: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+    },
+    colors: ['#FFB836', '#7B61FF'],
+    series: [{
+        name: 'Job Viewed',
+        data: [44, 55, 41, 67, 22, 43, 30]
+    }, {
+        name: 'Job Applied',
+        data: [13, 23, 20, 8, 13, 27, 70]
+    }],
     legend: {
-        show: false
+        show: true
     },
     tooltip: {
         enabled: false,
     },
-    responsive: [{
-        breakpoint: 3000,
-        options: {
-            chart: {
-                width: 170
-            },
+}
+
+// chart
+const chartOptions_2 = {
+    chart: {
+        type: 'bar',
+        stacked: true,
+    },
+    plotOptions: {
+        bar: {
+            horizontal: true,
+            dataLabels: {
+                total: {
+                    enabled: false,
+                    offsetX: 0,
+                    style: {
+                        fontSize: '13px',
+                        fontWeight: 900
+                    }
+                }
+            }
+        },
+    },
+    dataLabels: {
+        enabled: false
+    },
+    states: {
+        hover: {
+            enabled: false,
         }
     },
-    {
-        breakpoint: 480,
-        options: {
-            chart: {
-                width: 100
-            },
-        }
-    }]
+    xaxis: {
+        categories: ['Mon']
+    },
+    colors: ['#7B61FF'],
+    series: [{
+        name: 'Marine Sprite',
+        data: [44, 55, 41, 37, 22, 43, 21]
+    },],
+    legend: {
+        show: false
+    },
+    tooltip: {
+        enabled: true,
+    },
 }
 
 </script>
 
 <style lang="css" scoped>
-.card {
-    /* border-radius: 0px; */
-    border: none;
-    height: 100%;
-}
-
-
 .colored-card {
     color: #fff;
     font-weight: bold;
+    border: none;
 }
 
 .colored-card .big-number {
@@ -193,13 +335,17 @@ const chartOptions = {
     font-weight: 600;
 }
 
-.total-jobs-card {
-    background-color: #4307C2;
+.card-color1 {
+    background-color: #873A70;
 
 }
 
-.interviewed-card {
-    background-color: #CE1DA7;
+.card-color2 {
+    background-color: #4307C2;
+}
+
+.card-color3 {
+    background-color: #1EB0B4;
 }
 
 .color-green {
@@ -208,5 +354,30 @@ const chartOptions = {
 
 .color-faint {
     color: #E9EBFD;
+}
+
+.info-card-tab1 .float-icon-card {
+    /* background-color: #FFB836; */
+    color: #ffffff;
+    width: 25px;
+    height: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    font-size: 11px;
+}
+
+.info-card-tab1 .big-number {
+    font-size: 1.6rem;
+    font-weight: 600;
+}
+
+.info-card-tab1 .color1 {
+    background-color: #FFB836;
+}
+
+.info-card-tab1 .color2 {
+    background-color: #7B61FF;
 }
 </style>

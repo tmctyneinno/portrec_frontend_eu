c4c0beee6f98a90a69cafbb31132af84ca56b2d5
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-07-22 11:00:22 +0100

    comment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap'
 
-
+  
 //bootstrap-icons
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
